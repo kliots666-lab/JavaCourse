032d6b0f5b8c9eaea61aa233ea425a2715b557cf
kliots666-lab <kliots666@gmail.com>
2026-04-03 17:28:11 +0500

    And Or

---
+++ src/Sintax/Lesson9_And_Or/And_Or.java
@@ -0,0 +1,30 @@
+package Sintax.Lesson9_And_Or;
+
+public class And_Or {
+    static void main() {
+
+        int a=10, b=3;
+
+        boolean isEqual = a == b;
+        boolean isGreater = a > b;
+        boolean isLess = a < b;
+        boolean isGreaterOrEqual = a >= b;
+        boolean isLessOrEqual = a <= b;
+        boolean isNotEqual = a != b;
+
+//        And(&&) Or(||)
+        boolean x=a>b;
+        boolean y=a==b;
+        System.out.println(x && y);
+        System.out.println(x || y);
+
+
+
+
+
+
+
+
+
+    }
+}

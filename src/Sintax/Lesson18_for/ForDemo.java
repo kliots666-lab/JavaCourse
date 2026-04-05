8b4f71681ac15778c236fba3075a59653ad8fc49
kliots666-lab <kliots666@gmail.com>
2026-04-05 10:37:31 +0500

    for

---
+++ src/Sintax/Lesson18_for/ForDemo.java
@@ -0,0 +1,16 @@
+package Sintax.Lesson18_for;
+
+public class ForDemo {
+    static void main() {
+
+        int y = 0;
+        for (int i = 0; i <= 10; i++) {
+            y += i;
+            //System.out.println(y);
+
+
+        }
+        System.out.println("y = "+y);
+
+    }
+}

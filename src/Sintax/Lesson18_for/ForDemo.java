ac52ce9923f8874811b7751856f740f2a5703ccd
kliots666-lab <kliots666@gmail.com>
2026-04-05 10:38:05 +0500

    for

--- src/Sintax/Lesson18_for/ForDemo.java
+++ src/Sintax/Lesson18_for/ForDemo.java
@@ -2,7 +2,7 @@ package Sintax.Lesson18_for;
 
 public class ForDemo {
     static void main() {
-
+//Расчет суммы от 0 до 10
         int y = 0;
         for (int i = 0; i <= 10; i++) {
             y += i;
